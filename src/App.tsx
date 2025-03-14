9f9264a0cd967fb8a362eef6bb541fc5a2224b1b
Your Name <your.email@example.com>
2025-03-13 23:22:34 -0700

    Switch to relative imports for better compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import { Box } from '@chakra-ui/react'
-import Navbar from 'components/Navbar'
-import Hero from 'sections/Hero'
-import About from 'sections/About'
-import Skills from 'sections/Skills'
-import Projects from 'sections/Projects'
-import Contact from 'sections/Contact'
+import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Skills from './sections/Skills'
+import Projects from './sections/Projects'
+import Contact from './sections/Contact'
 
 const App: React.FC = () => {
   return (
